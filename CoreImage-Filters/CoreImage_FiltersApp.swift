53ea6a7add79dba23b2992c7b3b095172a7bd2ce
Ibrahim Hassan <mdibrahimhassan@gmail.com>
2023-04-07 21:05:45 +0530

    Initial Commit

---
+++ CoreImage-Filters/CoreImage_FiltersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreImage_FiltersApp.swift
+//  CoreImage-Filters
+//
+//  Created by Ibrahim Hassan on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreImage_FiltersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
